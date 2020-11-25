73e6c87d8c9529af910b0b4959e759c090a5ea8b
Guy Sartorelli <guy.sartorelli@signify.co.nz>
2020-11-25 16:29:35 +1300

    Install java

--- userdata.sh
+++ userdata.sh
@@ -5,7 +5,7 @@ wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 yum upgrade -y
-yum install jenkins -y
+yum install jenkins java-1.8.0-openjdk-devel -y
 
 # Disable setup wizard
 old_java_options='JENKINS_JAVA_OPTIONS="-Djava.awt.headless=true'

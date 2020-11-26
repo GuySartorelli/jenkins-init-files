c28c0a6bb9227339673476f4190d4f5458ea2913
Guy Sartorelli <guy.sartorelli@signify.co.nz>
2020-11-26 13:02:28 +1300

    Set a port if one is passed into the script.

--- userdata.sh
+++ userdata.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+HTTP_PORT=$1
+
 # Install jenkins
 wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
@@ -11,8 +13,10 @@ yum install jenkins java-1.8.0-openjdk-devel -y
 old_java_options='JENKINS_JAVA_OPTIONS="-Djava.awt.headless=true'
 new_java_options='$old_java_options -Djenkins.install.runSetupWizard=false'
 sed -i 's/$old_java_options/$new_java_options/' /etc/sysconfig/jenkins
-# Set port
-#sed -i 's/JENKINS_PORT="8080"/JENKINS_PORT="80"/' /etc/sysconfig/jenkins
+# Set port if passed in
+if [[ ! -z $HTTP_PORT ]]; then
+  sed -i 's/JENKINS_PORT="8080"/JENKINS_PORT="$HTTP_PORT"/' /etc/sysconfig/jenkins
+fi
 
 # Get groovy scripts
 groovydir=/var/lib/jenkins/init.groovy.d

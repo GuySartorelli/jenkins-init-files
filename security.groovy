d85dd2f2accf9df6b775a9c61d2aa17a3fb0bcca
Guy Sartorelli <guy.sartorelli@signify.co.nz>
2020-11-30 14:54:46 +1300

    Replace println with logging.

--- security.groovy
+++ security.groovy
@@ -5,27 +5,31 @@ import hudson.security.*
 import jenkins.security.s2m.AdminWhitelistRule
 import hudson.security.csrf.DefaultCrumbIssuer
 import jenkins.model.Jenkins
+import java.util.logging.Logger
 
+def logger = Logger.getLogger("")
 def instance = Jenkins.get()
 
 // Create a default admin user
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
 hudsonRealm.createAccount("admin", "admin")
 instance.setSecurityRealm(hudsonRealm)
+logger.info("Default administrator account created.")
 
 // Don't allow anonymous users to access anything
 def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
 strategy.setAllowAnonymousRead(false)
 instance.setAuthorizationStrategy(strategy)
 instance.save()
+logger.info("Initial security strategy set.")
 
 //instance.getInjector().getInstance(AdminWhitelistRule.class)
 
 if(instance.getCrumbIssuer() == null) {
     instance.setCrumbIssuer(new DefaultCrumbIssuer(true))
     instance.save()
-    println 'CSRF Protection configuration has changed.  Enabled CSRF Protection.'
+    logger.info("CSRF Protection configuration has changed.  Enabled CSRF Protection.")
 }
 else {
-    println 'CSRF Protection already configured.'
+    logger.info("CSRF Protection already configured.")
 }
